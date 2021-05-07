8f155637e406b408789bb905773d25a8c8b51eb6
Lelolaleilo <64855766+Lelolaleilo@users.noreply.github.com>
2021-05-07 00:35:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,21 @@ window.onscroll = function(){
         buttonUp.style.transform = "scale(1)";
     }else if(scroll < 100){
         buttonUp.style.transform = "scale(0)";
+    
     }
 
 }
+
+ 
+
+$('ul a').click(function(e){
+    var time=500
+    e.preventDefault();		//evitar el eventos del enlace normal
+    var strAncla=$(this).attr('href'); //id del ancla
+        $('body,html').stop(true,true).animate({				
+            scrollTop: $(strAncla).offset().top },time);
+    
+});
+
 
 
