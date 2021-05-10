aa35a1e831374632ab338ae75ffe5dbbc8fbcbc0
Lelolaleilo <64855766+Lelolaleilo@users.noreply.github.com>
2021-05-10 13:28:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function scrollUp(){
 
     if (currentScroll > 0){
         window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 20));
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
     }
 }
 
